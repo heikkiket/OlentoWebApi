ff383c489c6466c2ca7d4f89cb5e30e1e119cd20
Matti Hyvönen <mattihyvonen@gmail.com>
2017-07-14 14:35:59 +0300

    Controllerin datatyypit ja muotoilu

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -7,59 +7,37 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace OlentoWebApi.Controllers
 {
-	[Route("api/[controller]")]
+    [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        //tee väristä JSON-objekti
+        string rgbAsJSON(uint r, uint g, uint b) {
+            return(
+                "{"
+                + "\"r\":" + r.ToString() + ", "
+                + "\"g\":" + g.ToString() + ", "
+                + "\"b\":" + b.ToString()
+                + "}"
+                );
+        }
+        
+        
+        //esim. GET api/values?x=5&y=3
+        [HttpGet]
+        public string Get(float x = 0, float y = 0)
+        {
+            //lasketaan väri koordinaattien mukaan
+            uint r = (uint)x;
+            uint g = (uint)y;
+            uint b = r + g;
+            
+            //värin rajatarkistus: r, g, b = {0...255}
+            if(r > 255)	r = 255;
+            if(g > 255)	g = 255;
+            if(b > 255)	b = 255;
 
-		//tee väristä JSON-objekti
-		string rgbAsJSON(uint r, uint g, uint b) {
-			return(
-				"{"
-				+ "\"r\":" + r.ToString() + ", "
-				+ "\"g\":" + g.ToString() + ", "
-				+ "\"b\":" + b.ToString()
-				+ "}"
-				);
-		}
-
-		
-		//esim. GET api/values?x=5&y=3
-		[HttpGet]
-		public string Get(uint x = 0, uint y = 0)
-		{
-			//lasketaan väri koordinaattien mukaan
-			uint r = x;
-			uint g = y;
-			uint b = x + y;
-			
-			//värin rajatarkistus: r, g, b = {0...255}
-			if(r > 255)	r = 255;
-			if(g > 255)	g = 255;
-			if(b > 255)	b = 255;
-
-			//palautetaan väri json-muodossa
-			return rgbAsJSON(r,g,b);
-		}
-
-		
-		//esim. POST api/values
-		[HttpPost]
-		public void Post([FromBody]string value)
-		{
-		}
-
-		
-		//esim. PUT api/values/5
-		[HttpPut("{id}")]
-		public void Put(int id, [FromBody]string value)
-		{
-		}
-
-		
-		//esim. DELETE api/values/5
-		[HttpDelete("{id}")]
-		public void Delete(int id)
-		{
-		}
-	}
+            //palautetaan väri json-muodossa
+            return rgbAsJSON(r,g,b);
+        }
+    }
 }
