8c15b3b0b24e1d80da1959fe0f82785999e786d2
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2017-11-03 17:38:20 +0200

    Testing deploy workflow with MVC backend

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -28,8 +28,8 @@ namespace OlentoWebApi.Controllers
         {
             //lasketaan väri koordinaattien mukaan
             uint r = (uint)x;
-            uint g = (uint)y;
-            uint b = r + g;
+            uint b = (uint)y;
+            uint g = r + b;
             
             //värin rajatarkistus: r, g, b = {0...255}
             if(r > 255)	r = 255;
